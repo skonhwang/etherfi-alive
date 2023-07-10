d7b929bd6e4d5008bb9c6ea7558c02ececebec22
skonhwang <seungkon.hwang@dsrvlabs.com>
2023-07-10 17:11:01 +0900

    Add flags

--- main.go
+++ main.go
@@ -16,20 +16,26 @@ import (
 
 const (
 	// Default values.
-	defaultAddr = "127.0.0.1"
-	defaultPort = 9002
+	defaultAddr     = "127.0.0.1"
+	defaultPort     = 9002
+	dirSyncClientV2 = "/root/bin/etherfi/sync-client-v2/"
+	nameSyncClient  = "etherfi-sync-clientv2"
 
 	pluginName = "vatz-plugin-etherfi-alive"
 )
 
 var (
-	addr string
-	port int
+	addr     string
+	port     int
+	dirSync  string
+	nameSync string
 )
 
 func init() {
 	flag.StringVar(&addr, "addr", defaultAddr, "IP Address(e.g. 0.0.0.0, 127.0.0.1)")
 	flag.IntVar(&port, "port", defaultPort, "Port number, default 9091")
+	flag.StringVar(&dirSync, "dirsync", dirSyncClientV2, "Location of etherfi-sync-clientv2")
+	flag.StringVar(&nameSync, "namesync", nameSyncClient, "Process name of etherfi-sync-client")
 
 	flag.Parse()
 }
@@ -45,61 +51,12 @@ func main() {
 	}
 }
 
-/*
-func query_gql() (string, bool) {
-	var retString string
-	var retBool bool
-	// create a client (safe to share across requests)
-	client := graphql.NewClient("https://api.studio.thegraph.com/query/41778/etherfi-mainnet/0.0.3")
-
-	// make a request
-	req := graphql.NewRequest(`
-		query {
-				bids(where: { status: "WON", bidderAddress: "0x7C0576343975A1360CEb91238e7B7985B8d71BF4" }) {
-					id
-				}
-			}
-		`)
-	// run it and capture the response
-	var respData map[string]interface{}
-	if err := client.Run(context.Background(), req, &respData); err != nil {
-		log.Fatal(err)
-	}
-	bids, ok := respData["bids"]
-	if !ok {
-		fmt.Println("bids not found in respData")
-	} else {
-		for _, bid := range bids.([]interface{}) {
-			bidMap := bid.(map[string]interface{})
-			if idValue, ok := bidMap["id"]; ok {
-				path := fmt.Sprintf("/Users/hwangseungkon/dsrv/2022/etherfi/sync-client-v2/etherfi-sync-clientv2/mnt/etherfi/sync_client_validator_keys/%s", idValue.(string))
-				_, errDir := os.Stat(path)
-				if os.IsNotExist(errDir) {
-					retString += fmt.Sprintf("%s is new one\n", idValue.(string))
-					fmt.Println(retString)
-					retBool = true
-				} else {
-					retString += fmt.Sprintf("%s is existed!\n", path)
-					fmt.Println(retString)
-					retBool = false
-				}
-			} else {
-				retString = "ID not found in bid"
-				retBool = false
-			}
-		}
-	}
-
-	fmt.Println(retString)
-	return retString, retBool
-}
-*/
-
 func checkEtherfiSyncClientv2() bool {
 	retBool := false
-	cmd := exec.Command("pgrep", "-x", "etherfi_sc_v2")
+	//cmd := exec.Command("pgrep", "-x", "etherfi_sc_v2")
+	fmt.Println("nameSync: ", nameSync)
+	cmd := exec.Command("pgrep", "-x", nameSync)
 	out, err := cmd.Output()
-	fmt.Println("asdfkjalsdkfjalskdfj")
 	if err != nil && err.Error() != "exit status 1" {
 		fmt.Println("Error: ", err)
 		return retBool
@@ -107,9 +64,12 @@ func checkEtherfiSyncClientv2() bool {
 
 	if len(out) == 0 {
 		fmt.Println("etherfi_sc_v2 is not running")
-
-		cmd := exec.Command("./etherfi_sc_v2", "listen")
-		cmd.Dir = "/Users/hwangseungkon/dsrv/2022/etherfi/sync-client-v2/"
+		process := nameSync
+		fmt.Println("Process name = ", nameSync)
+		//cmd := exec.Command("./etherfi_sc_v2", "listen")
+		cmd := exec.Command(process, "listen")
+		//cmd.Dir = "/Users/hwangseungkon/dsrv/2022/etherfi/sync-client-v2/"
+		cmd.Dir = dirSync
 		var stderr bytes.Buffer
 		cmd.Stderr = &stderr
 		err := cmd.Run()
@@ -131,19 +91,7 @@ func pluginFeature(info, option map[string]*structpb.Value) (sdk.CallResponse, e
 	severity := pluginpb.SEVERITY_INFO
 	state := pluginpb.STATE_NONE
 	// TODO: Fill here.
-	/*
-		str, isNew := query_gql()
-		if isNew {
-			severity = pluginpb.SEVERITY_CRITICAL
-			state = pluginpb.STATE_SUCCESS
-			fmt.Println(str)
-		} else {
-			str = "There is no new one."
-			severity = pluginpb.SEVERITY_INFO
-			state = pluginpb.STATE_SUCCESS
-			fmt.Println(str)
-		}
-	*/
+	fmt.Println("Check process!!")
 	chkProcess := checkEtherfiSyncClientv2()
 	var str string
 	if chkProcess == false {
